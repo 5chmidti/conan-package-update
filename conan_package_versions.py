597245d6b6d33a537cefab881c042168d201131a
5chmidti <44101708+5chmidti@users.noreply.github.com>
2023-07-19 02:16:55 +0200

    add option to confirm each available packge update

--- conan_package_versions.py
+++ conan_package_versions.py
@@ -9,7 +9,7 @@ from packaging import version
 from rich.prompt import Confirm
 from rich.logging import RichHandler
 import logging
-from argparse import ArgumentParser
+from argparse import ArgumentParser, BooleanOptionalAction
 
 FORMAT = "%(message)s"
 logging.basicConfig(
@@ -26,8 +26,8 @@ def get_requires_lists(conan_package: ModuleType) -> list[str]:
             instance = potential_class()
             if "requirements" in dir(instance):
                 instance.requirements()
-            if "built_requirements" in dir(instance):
-                instance.built_requirements()
+            if "build_requirements" in dir(instance):
+                instance.build_requirements()
             res += map(lambda x: repr(x.ref), instance.requires.values())
     log.info(f"requirements: {res}")
     return res
@@ -86,10 +86,17 @@ def init_argparse() -> ArgumentParser:
         type=str,
         nargs="+",
     )
+    parser.add_argument(
+        "--single",
+        "-s",
+        help="interative update on a per package basis",
+        type=bool,
+        action=BooleanOptionalAction
+    )
     return parser
 
 
-def run_for_project(project_path: str):
+def run_for_project(project_path: str, single:bool):
     log.info(f"checking {project_path}")
     if not has_conanfile(project_path):
         log.info(f"no conanfile found in {project_path}")
@@ -109,8 +116,14 @@ def run_for_project(project_path: str):
     if updates:
         log.info(f"updates found for {project_path}")
         log.info(f"updates: {updates}")
-        if Confirm.ask("update conanfile?"):
-            update_conanfile(project_path, updates)
+        if single:
+            for update in updates:
+                if Confirm.ask(f"update package {update[0]} from {update[1]} to {update[2]}?"):
+                    update_conanfile(project_path,[update])
+        else:
+            if Confirm.ask("update conanfile?"):
+                update_conanfile(project_path, updates)
+
     path.remove(project_path)
 
 
@@ -130,5 +143,5 @@ if __name__ == "__main__":
 
     project_path: list[str] = args.paths
     for project in project_path:
-        run_for_project(get_folder_path(project))
+        run_for_project(get_folder_path(project),args.single)
 
